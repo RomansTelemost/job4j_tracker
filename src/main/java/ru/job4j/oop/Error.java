e73a7c92496596a528957b359671b41df50b83e2
Roman Koshelev <termopsis@mail.ru>
2022-12-28 20:51:29 +0300

    1.7. Конструктор [#173889] task

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Активный: " + active);
+        System.out.println("Статус: " + status);
+        System.out.println("Сообщение: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error defaultError = new Error();
+        defaultError.printInfo();
+        Error error404 = new Error(true, 404, "Not found");
+        error404.printInfo();
+        Error error500 = new Error(true, 500, "Internal server error");
+        error500.printInfo();
+    }
+}
