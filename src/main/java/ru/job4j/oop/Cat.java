55eed414514335416cc379b96c65a7f384f3375f
Roman Koshelev <termopsis@mail.ru>
2022-12-28 17:04:05 +0300

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -2,14 +2,34 @@ package ru.job4j.oop;
 
 public class Cat {
 
+    private String name;
+    private String food;
+
+    public void show() {
+        System.out.println("My name is " + this.name + " and I ate " + this.food);
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
     public String sound() {
         String voice = "may-may";
         return voice;
     }
 
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        String say = peppy.sound();
-        System.out.println(say);
+        Cat gav = new Cat();
+        gav.giveNick("gav");
+        gav.eat("kotleta");
+        gav.show();
+        Cat black = new Cat();
+        black.giveNick("black cat");
+        black.eat("fish");
+        black.show();
     }
 }
