1b9a61677a8d7ff64dd5ef10bcb28e4507abca03
Roman Koshelev <termopsis@mail.ru>
2022-12-30 01:04:54 +0300

    6.2. Локальные и анонимные внутренние классы [#504952 #376136]

---
+++ src/main/java/ru/job4j/oop/Local.java
@@ -0,0 +1,38 @@
+package ru.job4j.oop;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Local {
+
+    private String name = "Petr";
+
+    public Local(String name) {
+        this.name = name;
+    }
+
+    /**
+     * We cannot change local variable define in local class. But can change variable of upper class.
+     */
+    public void getFullName() {
+        final String surname = "Arsentev";
+
+        class FullName {
+
+            public void printFullName() {
+                Local.this.name = "Changed name from local class";
+                System.out.println(name + " " + surname);
+            }
+        }
+
+        FullName fullName = new FullName();
+        fullName.printFullName();
+    }
+
+    public static void main(String[] args) {
+        Local local = new Local("Roman");
+        local.getFullName();
+        local.name = "New name";
+        local.getFullName();
+    }
+
+}
