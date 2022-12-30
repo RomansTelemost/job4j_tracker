3a4da5d1df04c7c307ca5fd270bac65849683ff2
Roman Koshelev <termopsis@mail.ru>
2022-12-30 22:26:14 +0300

    7. Аннотация @Override [#174891]

--- src/main/java/ru/job4j/inheritance/HtmlReport.java
+++ src/main/java/ru/job4j/inheritance/HtmlReport.java
@@ -2,22 +2,10 @@ package ru.job4j.inheritance;
 
 public class HtmlReport extends TextReport {
 
-    public static void main(String[] args) {
-        HtmlReport report = new HtmlReport();
-        String text = report.generate("Report's name", "Report's body");
-        System.out.println(text);
-        System.out.println(report.testStaticMethodOverriding("sss"));
-    }
-
     @Override
     public String generate(String name, String body) {
         return "<h1>" + name + "</h1>"
                 + "<br/>"
                 + "<span>" + body + "</span>";
-    }
-
-    public static int testStaticMethodOverriding(String s) {
-        System.out.println(s);
-        return 2;
     }
 }
--- src/main/java/ru/job4j/inheritance/TextReport.java
+++ src/main/java/ru/job4j/inheritance/TextReport.java
@@ -5,10 +5,4 @@ public class TextReport {
     public String generate(String name, String body) {
         return name + System.lineSeparator() + body;
     }
-
-    public static int testStaticMethodOverriding(String sss) {
-        System.out.println(sss);
-        return 1;
-    }
-
 }
