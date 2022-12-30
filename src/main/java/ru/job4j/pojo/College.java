bc3f556b41d12f841049f61dda103d8ecb040712
Roman Koshelev <termopsis@mail.ru>
2022-12-30 23:06:46 +0300

    2. Модель данных. [#174683] - task

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,18 @@
+package ru.job4j.pojo;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class College {
+
+    public static void main(String[] args) throws Exception {
+        Student student = new Student();
+        student.setFullName("Petr Petrov");
+        student.setGroup("FOIT");
+        student.setAdmissionDate(LocalDate.parse("09-10-2013", DateTimeFormatter.ofPattern("dd-MM-yyyy", Locale.UK)));
+        System.out.println("Full name: " + student.getFullName()
+                + " Group : " + student.getGroup()
+                + " Admission date : " + student.getAdmissionDate().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
+    }
+}
