d83516da12b1a092c005e49de135c6586c323d65
Roman Koshelev <termopsis@mail.ru>
2023-01-10 21:09:45 +0300

    12.2. Тесты вывода на консоль [#33585]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -72,4 +72,59 @@ class StartUITest {
                         + "=== Exit Program ===" + System.lineSeparator()
         );
     }
+
+
+    @Test
+    public void whenReplaceItemTestOutputIsSuccessfully() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item one = tracker.add(new Item("test1"));
+        String replaceName = "New Test Name";
+        Input in = new StubInput(
+                new String[] {"0", String.valueOf(one.getId()), replaceName, "1"}
+        );
+        UserAction[] actions = new UserAction[]{
+                new EditAction(out),
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString()).isEqualTo(
+                "Menu." + ln
+                        + "0. Edit item" + ln
+                        + "1. Exit" + ln
+                        + "=== Edit item ===" + ln
+                        + "Заявка изменена успешно." + ln
+                        + "Menu." + ln
+                        + "0. Edit item" + ln
+                        + "1. Exit" + ln
+                        + "=== Exit Program ===" + ln
+        );
+    }
+
+    @Test
+    public void whenFindAllItems() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item one = tracker.add(new Item("Example"));
+        String[] answers = {"0", String.valueOf(one.getId()), "1"};
+        Input input = new StubInput(answers);
+        UserAction[] actions = {
+                new DeleteAction(out),
+                new ExitAction(out)
+        };
+        new StartUI(out).init(input, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString()).isEqualTo(
+                "Menu." + ln
+                + "0. Delete item" + ln
+                + "1. Exit" + ln
+                + "=== Delete item ===" + ln
+                + "Заявка удалена успешно." + ln
+                + "Menu." + ln
+                + "0. Delete item" + ln
+                + "1. Exit" + ln
+                + "=== Exit Program ===" + ln
+        );
+    }
 }
