08b56506bf036bfb2fe50a97a779725ea105dd75
Roman Koshelev <termopsis@mail.ru>
2023-01-11 20:16:01 +0300

    3. final [#334073]

---
+++ src/main/java/ru/job4j/ooa/FinalUsage.java
@@ -0,0 +1,11 @@
+package ru.job4j.ooa;
+
+public class FinalUsage {
+
+    static final String NAME = "Petr";
+
+    public static void main(String[] args) {
+        final String NAME = "Petr";
+        System.out.println(NAME);
+    }
+}
---
+++ src/main/java/ru/job4j/ooa/Toyota.java
@@ -0,0 +1,4 @@
+package ru.job4j.ooa;
+
+public class Toyota extends Car {
+}
