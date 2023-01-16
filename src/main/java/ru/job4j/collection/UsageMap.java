839c354f818d51a82632fa4209b8ec8269b1f436
Roman Koshelev <termopsis@mail.ru>
2023-01-17 00:37:53 +0300

    0. Map, HashMap [#212652] - cycle by entrySet()

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -1,6 +1,7 @@
 package ru.job4j.collection;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class UsageMap {
 
@@ -11,6 +12,10 @@ public class UsageMap {
         for (String key : map.keySet()) {
             System.out.println(key);
             System.out.println(map.get(key));
+        }
+        for (Map.Entry<String, String> pair : map.entrySet()) {
+            System.out.println(pair.getKey());
+            System.out.println(pair.getValue());
         }
     }
 }
