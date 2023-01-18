90320e23bcced6addb3e2ace84095cda9b34fc55
Roman Koshelev <termopsis@mail.ru>
2023-01-19 02:03:19 +0300

    0. Сортировка [#10096 #382606] - task - fix 1

--- src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
+++ src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
@@ -12,19 +12,15 @@ class ItemAscByNameTest {
 
     @Test
     public void whenItemsSortAscByName() {
-        Item item1 = new Item(1, "Request 1");
-        Item item3 = new Item(3, "Request 3");
-        Item item5 = new Item(5, "Request 5");
-
         List<Item> expected = new ArrayList<>();
-        expected.add(item1);
-        expected.add(item3);
-        expected.add(item5);
+        expected.add(new Item(1, "Request 1"));
+        expected.add(new Item(3, "Request 3"));
+        expected.add(new Item(5, "Request 5"));
 
         List<Item> list = new ArrayList<>();
-        list.add(item1);
-        list.add(item3);
-        list.add(item5);
+        list.add(new Item(3, "Request 3"));
+        list.add(new Item(1, "Request 1"));
+        list.add(new Item(5, "Request 5"));
         Collections.sort(list, new ItemAscByName());
         assertThat(list).containsAll(expected);
     }
--- src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
+++ src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
@@ -12,20 +12,16 @@ class ItemDescByNameTest {
 
     @Test
     public void whenItemsSortDescByName() {
-        Item item1 = new Item(1, "Request 1");
-        Item item3 = new Item(3, "Request 3");
-        Item item5 = new Item(5, "Request 5");
-
         List<Item> expected = new ArrayList<>();
-        expected.add(item5);
-        expected.add(item3);
-        expected.add(item1);
+        expected.add(new Item(5, "Request 5"));
+        expected.add(new Item(3, "Request 3"));
+        expected.add(new Item(1, "Request 1"));
 
         List<Item> list = new ArrayList<>();
-        list.add(item1);
-        list.add(item3);
-        list.add(item5);
-        Collections.sort(list, new ItemDescByName());
+        list.add(new Item(3, "Request 3"));
+        list.add(new Item(1, "Request 1"));
+        list.add(new Item(5, "Request 5"));
+        Collections.sort(list, new ItemAscByName());
         assertThat(list).containsAll(expected);
     }
 }
