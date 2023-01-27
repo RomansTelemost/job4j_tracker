3945cf553291157423a718dae88f212b9473d801
Roman Koshelev <termopsis@mail.ru>
2023-01-27 20:56:01 +0300

    6. Подсчет функции в диапазоне. [#24251]

---
+++ src/main/java/ru/job4j/lambda/FunctionCalculator.java
@@ -0,0 +1,16 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class FunctionCalculator {
+
+    public List<Double> diapason(int start, int end, Function<Double, Double> func) {
+        List<Double> doubles = new ArrayList<>();
+        for (double i = start; i < end; i++) {
+            doubles.add((double) Math.round(func.apply(i) * 100) / 100);
+        }
+        return doubles;
+    }
+}
---
+++ src/test/java/ru/job4j/lambda/FunctionCalculatorTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.lambda;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FunctionCalculatorTest {
+
+    @Test
+    public void whenLinearFunctionThenLinearResults() {
+        FunctionCalculator function = new FunctionCalculator();
+        List<Double> result = function.diapason(5, 8, x -> 2 * x + 1);
+        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        assertThat(result).containsAll(expected);
+    }
+
+    @Test
+    public void whenSquareFunctionThenSquareResults() {
+        FunctionCalculator function = new FunctionCalculator();
+        List<Double> result = function.diapason(5, 8, x -> 2 * Math.sqrt(x) + 6 * x + 9);
+        List<Double> expected = Arrays.asList(43.47D, 49.9D, 56.29D);
+        assertThat(result).containsAll(expected);
+    }
+
+    @Test
+    public void whenPowFunctionThenPowResults() {
+        FunctionCalculator function = new FunctionCalculator();
+        List<Double> result = function.diapason(5, 8, x -> Math.pow(2, x));
+        List<Double> expected = Arrays.asList(32D, 64D, 128D);
+        assertThat(result).containsAll(expected);
+    }
+}
