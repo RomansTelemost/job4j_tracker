7b67ec6811663655b75c843c9913cb21aac8a429
Roman Koshelev <termopsis@mail.ru>
2023-01-28 11:09:43 +0300

    6. Список адресов [#110062] - refactor

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -6,6 +6,8 @@ import java.util.stream.Collectors;
 public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(profile -> profile.getAddress()).collect(Collectors.toList());
+        return profiles.stream()
+                .map(profile -> profile.getAddress())
+                .collect(Collectors.toList());
     }
 }
