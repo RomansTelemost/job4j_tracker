b7dc8bbfcd13adc1b98643e37452103c26b41dc5
Roman Koshelev <termopsis@mail.ru>
2023-01-29 21:35:35 +0300

    13. Методы преобразования потоков объектов в потоки примитивы и наоборот [#504927 #386243]

---
+++ src/main/java/ru/job4j/stream/mapto/FlatMapToIntExample.java
@@ -0,0 +1,26 @@
+package ru.job4j.stream.mapto;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class FlatMapToIntExample {
+
+    public static void main(String[] args) {
+        int[] array1 = {1, 2, 3};
+        int[] array2 = {4, 5, 6};
+        int[] array3 = {7, 8, 9};
+        List<Integer> integerList = Stream.of(array1, array2, array3)
+                .flatMapToInt(Arrays::stream)
+                .boxed()
+                .collect(Collectors.toList());
+        System.out.println(integerList);
+        List<String> stringList = IntStream.rangeClosed(1, 5)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.toList());
+        System.out.println(stringList);
+
+    }
+}
