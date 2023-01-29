d605ff5087a472a610134d5ea2a316b25673418d
Roman Koshelev <termopsis@mail.ru>
2023-01-29 22:13:13 +0300

    10. Преобразование матрицы чисел в список чисел. FlatMap. [#110228 #386145] - additional

--- src/main/java/ru/job4j/stream/mapto/FlatMapToIntExample.java
+++ src/main/java/ru/job4j/stream/mapto/FlatMapToIntExample.java
@@ -21,6 +21,24 @@ public class FlatMapToIntExample {
                 .mapToObj(String::valueOf)
                 .collect(Collectors.toList());
         System.out.println(stringList);
-
+        List<String> list1 = List.of("1", "2", "3");
+        List<String> list2 = List.of("4", "5", "6");
+        List<String> list3 = List.of("7", "88", "99");
+        List<String> ss = Stream.of(list1, list2, list3)
+                .flatMap(strings -> strings.stream())
+                .collect(Collectors.toList());
+        System.out.println(ss);
+        List<String> ss1 = Stream.of(list1, list2)
+                .flatMap(strings -> strings.stream()).toList();
+        System.out.println(ss1);
+        String[] arr1 = new String[]{"1"};
+        String[] arr2 = new String[]{"2"};
+        String[] arr3 = new String[]{"3"};
+        stringList = Stream.of(arr1, arr2, arr3).flatMap(Arrays::stream).toList();
+        System.out.println(stringList);
+        List<String> s1 = Stream.of("a", "б")
+                .map(String::toUpperCase)
+                .toList();
+        System.out.println(s1);
     }
 }
