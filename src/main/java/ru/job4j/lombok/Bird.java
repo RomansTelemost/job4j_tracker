c4eaac08dfa3ac785bf250e7d29854d3f017cb0d
Roman <termopsis@mail.ru>
2024-01-05 16:07:40 +0300

    1. Lombok. Настройка IDEA [#504888] - lesson

--- src/main/java/ru/job4j/lombok/Bird.java
+++ src/main/java/ru/job4j/lombok/Bird.java
@@ -34,8 +34,12 @@ public class Bird {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Bird bird = (Bird) o;
         return age == bird.age
                 && wingspan == bird.wingspan
