442561596e006551a8fdb1630548f01d6831ab6d
Roman <termopsis@mail.ru>
2024-01-05 20:55:48 +0300

    2. @Setter, @Getter и другие [#504889]

--- src/main/java/ru/job4j/lombok/BirdData.java
+++ src/main/java/ru/job4j/lombok/BirdData.java
@@ -1,10 +1,21 @@
 package ru.job4j.lombok;
 
-import lombok.Data;
+import lombok.*;
 
-@Data
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class BirdData {
+    @Getter
+    @Setter
+    @NonNull
     private String color;
+    @Getter
+    @Setter
     private int age;
+    @Getter
+    @Setter
+    @EqualsAndHashCode.Include
     private int wingspan;
 }
