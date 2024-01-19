03a25b5203df572342ce2b1ce421e15428f0afa4
Roman <termopsis@mail.ru>
2024-01-20 00:59:17 +0700

    0. ToOne [#6873]

---
+++ src/main/java/ru/job4j/toone/User.java
@@ -0,0 +1,25 @@
+package ru.job4j.toone;
+
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "j_user")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int id;
+
+    @EqualsAndHashCode.Include
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+}
